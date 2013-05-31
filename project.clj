ee2616203316a3a6f83dd2a7c12190c944cba747
Nick Zalabak <techwhizbang@gmail.com>
2013-05-31 09:52:48 -0700

    updating some deps, and included logging

--- project.clj
+++ project.clj
@@ -5,7 +5,14 @@
             :url "http://en.wikipedia.org/wiki/MIT_License"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-yaml "0.4.0"]
-                 [clj-time "0.5.0"]
-                 [com.taoensso/carmine "1.6.0"]
-                 [cheshire "5.1.1"]]
+                 [clj-time "0.5.1"]
+                 [com.taoensso/carmine "1.8.0"] ; Redis client
+                 [cheshire "5.1.1"] ; JSON parsing
+
+                 ;; Logging dependencies
+                 [org.clojure/tools.logging "0.2.6"]
+                 [log4j "1.2.17" :exclusions [javax.mail/mail
+                                              javax.jms/jms
+                                              com.sun.jdmk/jmxtools
+                                              com.sun.jmx/jmxri]]]
   :profiles {:dev {:dependencies [[org.clojars.runa/conjure "2.1.3"]]}})
