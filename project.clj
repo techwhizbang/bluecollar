3acd32a3642ee21f8f6f12506432190fae7e595f
Nick Zalabak <techwhizbang@gmail.com>
2013-08-22 12:56:49 -0700

    beta5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bluecollar/bluecollar "1.0.0-beta4-SNAPSHOT"
+(defproject bluecollar/bluecollar "1.0.0-beta5-SNAPSHOT"
   :description "Bluecollar: a simple yet fully featured distributed background processing solution written in Clojure."
   :url "http://github.com/techwhizbang/bluecollar"
   :license {:name "Eclipse Public License"
